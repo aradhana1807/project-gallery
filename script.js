653a14d91c6f1a2ebb1370266820889902f418c7
aradhana1807 <aradhana.zero@gmail.com>
2024-03-12 17:14:26 +0530

    added grid style and onload effect for grids

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ window.addEventListener("scroll", () => {
     toTop.classList.remove("active");
   }
 })
+
+
+
+const gridItems = document.querySelectorAll('.item');
+
+
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach((entry, index) => {
+    if (entry.isIntersecting) {
+      setTimeout(() => {
+        entry.target.classList.add('fadeIn');
+      }, index * 200);
+      observer.unobserve(entry.target);
+    }
+  });
+});
+
+gridItems.forEach(item => {
+  observer.observe(item);
+});
