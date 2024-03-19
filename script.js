8cf8822a768210367b96d6e1ff6a9623aa06d8d5
aradhana1807 <aradhana.zero@gmail.com>
2024-03-19 20:17:04 +0530

    added loader

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
-const scroll = new LocomotiveScroll({
-  el: document.querySelector('.container'),
-  smooth: true
-});
+// const scroll = new LocomotiveScroll({
+//   el: document.querySelector('.container'),
+//   smooth: true
+// });
+
+function init() {
+  gsap.registerPlugin(ScrollTrigger);
+
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector(".container"),
+    smooth: true
+  });
+
+  locoScroll.on("scroll", ScrollTrigger.update);
+
+  ScrollTrigger.scrollerProxy(".container", {
+    scrollTop(value) {
+      return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+    },
+    getBoundingClientRect() {
+      return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
+    },
+    pinType: document.querySelector(".container").style.transform ? "transform" : "fixed"
+  });
+
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+  ScrollTrigger.refresh();
+}
+init();
+
 
 document.addEventListener('DOMContentLoaded', function () {
 
@@ -42,10 +69,38 @@ window.addEventListener("scroll", () => {
 
 const tl = gsap.timeline();
 
-tl.from('.item',
-  {
-    duration: 1,
-    stagger: 0.3,
-    opacity: 0,
-    y: 100
-  })
+function createTimeLoader() {
+  let randomValue = 0
+  setInterval(function () {
+    randomValue = randomValue + Math.floor(Math.random() * 20);
+    if (randomValue < 100) {
+      document.querySelector('#loader h1').innerText = randomValue + '%';
+    }
+    else {
+      randomValue = 100;
+      document.querySelector('#loader h1').innerText = randomValue + '%';
+    }
+  }, 100);
+}
+
+
+tl.to("#loader h1", {
+  scale: 1.5,
+  delay: 0.5,
+  duration: 1,
+  onStart: createTimeLoader()
+})
+
+tl.to("#loader", {
+  top: "-100vh",
+  delay: 0.3,
+  duration: 1.5
+})
+
+
+tl.from('.item', {
+  duration: 1,
+  stagger: 0.3,
+  opacity: 0,
+  y: 100
+})
